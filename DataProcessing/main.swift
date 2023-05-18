90c3d1be6bf3aad69f061a82ed5e93ee33754065
Moyses Azevedo <m.mirazev@gmail.com>
2023-05-17 21:26:20 -0300

    check point

--- DataProcessing/main.swift
+++ DataProcessing/main.swift
@@ -16,30 +16,45 @@ let imageManager = ImageManager()
 func main() {
     guard let dataSet = myFileManager.urlDataSet() else { return }
     guard let dataSetDebug = myFileManager.urlDataSetDebug() else { return }
-    guard let directorys = myFileManager.directorysInUrl(url: dataSet) else { return }
-
-    for directory in directorys {
+    guard let directories = myFileManager.directorysInUrl(url: dataSet) else { return }
+    for directory in directories {
         if directory.description.hasSuffix("/0/") {
-            guard let images = myFileManager.filesInUrl(url: directory) else { return }
+            filterImage(directory: directory, nameFolder: "0")
 
-            for imageURL in images {
-                guard let data = myFileManager.dataFromFile(at: imageURL) else { return }
-                guard let face = visionManager.detectFace(imageData: data) else {return}
-                guard let ciImage = CIImage(data: data) else { return }
+        } else if directory.description.hasSuffix("/1/") {
+            filterImage(directory: directory, nameFolder: "1")
 
-                let rect = face.boundingBox.convertNormalizedRect(imageSize: ciImage.extent.size)
-                let halfRect = CGRect(x: rect.origin.x, y: rect.origin.y + rect.size.height/2, width: rect.width, height: rect.height * 0.5)
-                guard let cropciImage = imageManager.cropImage(ciImage, toRect: halfRect) else { return }
+        } else if directory.description.hasSuffix("/2/") {
+            filterImage(directory: directory, nameFolder: "2")
 
-                guard let finalData = imageManager.convertCIImageToData(ciImage: cropciImage) else { return }
+        } else if directory.description.hasSuffix("/3/") {
+            filterImage(directory: directory, nameFolder: "3")
+
+        }
 
 
+    }
+}
 
-                
-            }
+private func filterImage(directory: URL, nameFolder: String) {
+    guard let images = myFileManager.filesInUrl(url: directory) else { return }
+    print(images.count)
+    for imageURL in images {
+        guard let data = myFileManager.dataFromFile(at: imageURL) else { return }
+        visionManager.detectFace(imageData: data) { face in
+            guard let ciImage = CIImage(data: data) else { return }
+
+            let rect = face.boundingBox.convertNormalizedRect(imageSize: ciImage.extent.size)
+            let halfRect = CGRect(x: rect.origin.x, y: rect.origin.y + rect.size.height/2, width: rect.width, height: rect.height * 0.5)
+            guard let cropciImage = imageManager.cropImage(ciImage, toRect: halfRect) else { return }
+
+            guard let finalData = imageManager.convertCIImageToData(ciImage: cropciImage) else { return }
+            myFileManager.savePng(imageData: finalData, nameImage: imageURL.lastPathComponent, nameFolder: nameFolder  )
         }
     }
 }
 
 
+main()
 
+RunLoop.main.run(until: .distantFuture)
--- Utils/VisionManager.swift
+++ Utils/VisionManager.swift
@@ -11,18 +11,18 @@ import CoreImage
 public class VisionManager {
     public init () {}
 
-    public func detectFace(imageData: Data) -> VNFaceObservation? {
-        guard let ciImage = CIImage(data: imageData) else { return nil }
-        let request = VNDetectFaceRectanglesRequest()
+    public func detectFace(imageData: Data, completion: @escaping (VNFaceObservation) -> Void) {
+        guard let ciImage = CIImage(data: imageData) else { return }
+        let request = VNDetectFaceRectanglesRequest { request, error in
+            if error == nil, let result = request.results?.first as? VNFaceObservation {
+                completion(result)
+            }
+        }
         let handler = VNImageRequestHandler(ciImage: ciImage)
         do {
             try handler.perform([request])
-
-            guard let result = request.results?.first as? VNFaceObservation else { return nil }
-            return result
         } catch {
             print(error.localizedDescription)
-            return nil
         }
     }
     
